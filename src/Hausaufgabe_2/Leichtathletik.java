fd132df2ba1d3f0b26fc95ae39da0268bc33ea49
Todiras Bogdan <bogdan.todiras@gmail.com>
2021-10-25 12:44:15 +0300

    Third Commit

--- src/Hausaufgabe_2/Leichtathletik.java
+++ src/Hausaufgabe_2/Leichtathletik.java
@@ -2,12 +2,6 @@ package Hausaufgabe_2;
 
 /**
  * Leichtathletik ist eine abstrakte Klasse, die das Interface Sport implementiert
- * und hat als Methoden nur kalkuliereZeit
  */
 public abstract class Leichtathletik implements Sport {
-
-    @Override
-    public double kalkuliereZeit() {
-        return kalkuliereZeit();
-    }
 }
--- src/Hausaufgabe_2/Manschaftssport.java
+++ src/Hausaufgabe_2/Manschaftssport.java
@@ -2,10 +2,6 @@ package Hausaufgabe_2;
 
 /**
  * Mannschaftssport ist eine abstrakte Klasse, die das Interface Sport implementiert
- * und hat als Methoden nur kalkuliereZeit
  */
 public abstract class Manschaftssport implements Sport {
-
-    @Override
-    public double kalkuliereZeit() { return kalkuliereZeit(); }
 }
